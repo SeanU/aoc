0c99589fa5018d4f518ed175e58ccd712320146c
Sean Underwood <flyinghamsterster@gmail.com>
2018-12-05 07:40:16 -0600

    Improving perf of day5puzzle2
    Not bothering with puzzle 1

--- day5/puzzle2.nim
+++ day5/puzzle2.nim
@@ -6,36 +6,29 @@ type
     Polymer = DoublyLinkedList[char]
     Monomer = DoublyLinkedNode[char]
 
+proc newPolymer(input: string): Polymer = 
+    result = initDoublyLinkedList[char]()
+    for character in input:
+        result.append(character)
+
 proc canReact(x, y: char): bool =
     return abs(int(x) - int(y)) == capDiff
-
-proc findReaction(polymer: Polymer, n1, n2: var Monomer): bool =
-    for node in polymer.nodes:
-        if node.next == nil:
-            return false
-    
-        if canReact(node.value, node.next.value):
-            n1 = node
-            n2 = node.next
-            return true
-
-    return false
 
 proc count(polymer: Polymer): int =
     for node in polymer.nodes:
         inc(result)
 
-proc newPolymer(input: string): Polymer =
-    var polymer: Polymer = initDoublyLinkedList[char]()
-    for character in input:
-        polymer.append(character)
-    polymer
-
 proc shrink(polymer: var Polymer): int =
-    var n1, n2: Monomer
-    while findReaction(polymer, n1, n2):
-        polymer.remove(n1)
-        polymer.remove(n2)
+    var shrank = true
+    while shrank:
+        shrank = false
+        for node in polymer.nodes:
+            if node.prev == nil:
+                continue
+    
+            if canReact(node.prev.value, node.value):
+                polymer.remove(node.prev)
+                polymer.remove(node)
     polymer.count()
 
 proc removeAll(polymer: var Polymer, unitType: char): void =
